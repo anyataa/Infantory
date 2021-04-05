5f9e15289d2219b8349083d7cdfbda93e2936f67
KarinLim <karinlinn18@gmail.com>
2021-04-05 13:03:37 +0700

    Initial Commit

---
+++ InfantoryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfantoryApp
+//
+//  Created by Karin Lim on 05/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
