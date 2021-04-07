18bcf488363b3711da4296b028b5a351fdb406a3
Elsha Danielle <elshaclarita@gmail.com>
2021-04-07 09:26:39 +0700

    Tambahin UI

--- InfantoryApp/Controller/BabyProfileFormController.swift
+++ InfantoryApp/Controller/BabyProfileFormController.swift
@@ -25,5 +25,29 @@ class BabyProfileFormController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+// Navigation Bar Outlets
+    @IBOutlet weak var addChildNavBar: UINavigationBar!
+    @IBOutlet weak var cancelButton: UINavigationItem!
+    @IBOutlet weak var doneButton: UINavigationItem!
+    
+// Add Photo Outlets
+    @IBOutlet weak var imageAddChild: UIImageView!
+    @IBOutlet weak var addPhotoButton: UIButton!
+    
+// Table View Outlets
+    @IBOutlet weak var tableViewAddChild: UITableView!
+    
+// Navigation Bar Actions
+    @IBAction func cancelButton(_ sender: Any) {
+    }
+    @IBAction func doneButton(_ sender: Any) {
+    }
+    
+// Add Photo Actions
+    @IBAction func addPhotoButton(_ sender: Any) {
+    }
+    
+// Table View Actions
+    
 }
