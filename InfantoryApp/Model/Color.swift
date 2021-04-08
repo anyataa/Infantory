90270b43dbd6dc643327f7b418d06d637fb74f17
KarinLim <karinlinn18@gmail.com>
2021-04-08 12:31:37 +0700

    karin add primary and secondary color

---
+++ InfantoryApp/Model/Color.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  InfantoryApp
+//
+//  Created by Karin Lim on 08/04/21.
+//
+
+import UIKit
+
+public extension UIColor{
+    static let primary = UIColor(named: "primary")
+    static let secondary = UIColor(named: "secondary")
+}
